681b05779645df2babce64181cf79604ffbf4694
itachyprodigio-a11y <itachyprodigio@gmail.com>
2025-10-20 11:42:09 -0500

    Add HasFactory trait to Product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory; // ✅ ESTA LÍNEA ES CLAVE
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
